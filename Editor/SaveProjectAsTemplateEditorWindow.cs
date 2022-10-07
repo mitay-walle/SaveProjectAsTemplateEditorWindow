657702d7d5fbd14c902dbc4bc4b59ded254182a8
Dmitry <mitay-walle91@mail.ru>
2022-10-07 23:07:38 +0300

    Update SaveProjectAsTemplateEditorWindow.cs

--- Editor/SaveProjectAsTemplateEditorWindow.cs
+++ Editor/SaveProjectAsTemplateEditorWindow.cs
@@ -34,6 +34,18 @@ namespace PumpEditor
         private SceneAsset templateDefaultSceneAsset;
         private bool replaceTemplate;
 
+        [MenuItem("Window/Pump Editor/Project Templates/Unity Project Template Window")]
+        private static void ShowWindow()
+        {
+            // Unity C# reference: https://github.com/Unity-Technologies/UnityCsReference/blob/73bda32d/Editor/Mono/ProjectTemplateWindow.cs#L11
+            Assembly editorAssembly = Assembly.GetAssembly(typeof(Editor));
+            Type projectTemplateWindowType = editorAssembly.GetType("UnityEditor.ProjectTemplateWindow");
+
+            // Unity C# reference: https://github.com/Unity-Technologies/UnityCsReference/blob/73bda32d/Editor/Mono/ProjectTemplateWindow.cs#L20
+            MethodInfo methodInfo = projectTemplateWindowType.GetMethod("SaveAsTemplate", BindingFlags.Static | BindingFlags.NonPublic);
+            methodInfo.Invoke(projectTemplateWindowType, null);
+        }
+
         [MenuItem("Window/Pump Editor/Project Templates/Save Project As Template")]
         private static void ShowWindow()
         {
